3c9389b31a98c0530d3fdcb9307fa034e55135a2
samed5266-ui <samed5266@gmail.com>
2026-02-04 10:11:00 +0300

    Refactor BootReceiver class for clarity

--- bootreceiver.java
+++ bootreceiver.java
@@ -7,10 +7,9 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Cihaz açıldığında tetiklenir
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Intent i = new Intent(context, MainActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Uygulamayı yeni bir görev olarak başlat
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(i);
         }
     }
